fd3f3b277d9fad0a7a17597f557091f1e56c2d3a
Marina Boyano <marina.boyano.gonzalez@gmail.com>
2025-07-01 21:33:22 +0200

    reparto ok

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./styles/App.scss";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Routes, Route } from "react-router";
+import { useState } from "react";
 
 function App() {
   const [projectData, setProjectData] = useState({
