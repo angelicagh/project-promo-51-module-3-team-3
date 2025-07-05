370b113e36a2b97dcc1fd08e768602d1184d27d5
sandramarincodes <smarinfernan@gmail.com>
2025-07-05 11:53:40 +0200

    Update React botones

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,10 @@
-import PropTypes from "prop-types";//por que salen tres puntitos?
+import PropTypes from "prop-types"; //por que salen tres puntitos?
 import GetAvatar from "./GetAvatar";
-
 
 // paso de App a Form la variable de estado y la funcion que la actualiza
 
-function Form({pprojectData, psetProjectData, /* pavatar, pupdateAvatar */}) {
-
-/*   const [avatar, setAvatar] = useState("")
+function Form({ pprojectData, psetProjectData /* pavatar, pupdateAvatar */ }) {
+  /*   const [avatar, setAvatar] = useState("")
   const updateAvatar = (avatar) => {
     console.log("nuevo avatar", avatar)
     setAvatar(avatar);
@@ -17,56 +15,134 @@ function Form({pprojectData, psetProjectData, /* pavatar, pupdateAvatar */}) {
     2. const value : guardo los datos del valor que escribe el usuario
     3. const newProjectData: creo una nueva constante para guardar los datos nuevos que va introduciendo el usuario (en un objeto)
     4. uso el spread operator, que quiere decir "deja lo que tienes en projectData y añade lo nuevo" (lo nuevo son los datos que va introduciendo el usuario)
-    5. actualiza la variable de estado original con los datos nuevos */ 
-    const handleChange = (ev) => {
-    const id = ev.target.id; 
-    const value = ev.target.value; 
+    5. actualiza la variable de estado original con los datos nuevos */
+  const handleChange = (ev) => {
+    const id = ev.target.id;
+    const value = ev.target.value;
     const newProjectData = {
-      ...pprojectData, [id]: value //spread operator
-    }
-    psetProjectData(newProjectData) 
-  }
+      ...pprojectData,
+      [id]: value, //spread operator
+    };
+    psetProjectData(newProjectData);
+  };
 
   return (
     <>
-    {/* todos los campos del formulario van asociados al mismo evento */}
+      {/* todos los campos del formulario van asociados al mismo evento */}
       <form className="addForm">
         <h2 className="title">Información</h2>
         <fieldset className="addForm__group">
-          <legend className="addForm__title">Cuéntanos sobre el proyecto</legend>
-          <input onChange={handleChange} className="addForm__input" type="text" name="name" id="name" placeholder="Nombre del proyecto" required/>
-          <input onChange={handleChange} className="addForm__input" type="text" name="slogan" id="slogan" placeholder="Slogan" required/>
+          <legend className="addForm__title">
+            Cuéntanos sobre el proyecto
+          </legend>
+          <input
+            onChange={handleChange}
+            className="addForm__input"
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Nombre del proyecto"
+            required
+          />
+          <input
+            onChange={handleChange}
+            className="addForm__input"
+            type="text"
+            name="slogan"
+            id="slogan"
+            placeholder="Slogan"
+            required
+          />
           <div className="addForm__2col">
-            <input onChange={handleChange} className="addForm__input" type="url" name="repo" id="repo" placeholder="Repositorio" required/>
-            <input onChange={handleChange} className="addForm__input" type="url" name="demo" id="demo" placeholder="Demo" required/>
-          </div>         
-          <input onChange={handleChange} className="addForm__input" type="text" name="technologies" id="technologies" placeholder="Tecnologías" required/>
-          <textarea onChange={handleChange} className="addForm__input" type="text" name="description" id="description" placeholder="Descripción" rows="5" required></textarea>
+            <input
+              onChange={handleChange}
+              className="addForm__input"
+              type="url"
+              name="repo"
+              id="repo"
+              placeholder="Repositorio"
+              required
+            />
+            <input
+              onChange={handleChange}
+              className="addForm__input"
+              type="url"
+              name="demo"
+              id="demo"
+              placeholder="Demo"
+              required
+            />
+          </div>
+          <input
+            onChange={handleChange}
+            className="addForm__input"
+            type="text"
+            name="technologies"
+            id="technologies"
+            placeholder="Tecnologías"
+            required
+          />
+          <textarea
+            onChange={handleChange}
+            className="addForm__input"
+            type="text"
+            name="description"
+            id="description"
+            placeholder="Descripción"
+            rows="5"
+            required
+          ></textarea>
         </fieldset>
-    
+
         <fieldset className="addForm__group">
           <legend className="addForm__title">Cuéntanos sobre la autora</legend>
-          <input onChange={handleChange} className="addForm__input" type="text" name="author" id="author" placeholder="Nombre" required/>
-          <input onChange={handleChange} className="addForm__input" type="text" name="job" id="job" placeholder="Trabajo" required/>
+          <input
+            onChange={handleChange}
+            className="addForm__input"
+            type="text"
+            name="author"
+            id="author"
+            placeholder="Nombre"
+            required
+          />
+          <input
+            onChange={handleChange}
+            className="addForm__input"
+            type="text"
+            name="job"
+            id="job"
+            placeholder="Trabajo"
+            required
+          />
         </fieldset>
-    
 
-       {/*  BOTONES */}
+        {/*  BOTONES */}
         <fieldset className="addForm__group--upload">
           {/* <label htmlFor="image" className="button">Subir foto del proyecto</label>
           <input className="addForm__hidden" type="file" name="image" id="image"/> */}
-          
-          <GetAvatar avatar={pprojectData.image}
-          updateAvatar={(image) => psetProjectData({ ...pprojectData, image })} text="Subir foto del proyecto"/>
+
+          <GetAvatar
+            avatar={pprojectData.image}
+            updateAvatar={(image) =>
+              psetProjectData({ ...pprojectData, image })
+            }
+            text="Subir foto del proyecto"
+          />
+          <GetAvatar
+            avatar={pprojectData.photo}
+            updateAvatar={(photo) =>
+              psetProjectData({ ...pprojectData, photo })
+            }
+            text="Subir foto de la autora"
+          />
 
           {/* <label htmlFor="photo" className="button">Subir foto de la autora</label>
           <input className="addForm__hidden" type="file" name="photo" id="photo"/>
           <button className="button--large">Crear proyecto</button> */}
         </fieldset>
-        
       </form>
-      </>
-  )
+    </>
+  );
 }
 
 //esto no termino de entenderlo
@@ -83,12 +159,6 @@ Form.propTypes = {
     job: PropTypes.string.isRequired,
   }).isRequired,
   psetProjectData: PropTypes.func.isRequired,
-}
+};
 
-
-export default Form
-
-
-
-
-
+export default Form;
--- src/components/ProjectPreview.jsx
+++ src/components/ProjectPreview.jsx
@@ -1,38 +1,67 @@
+import defaultImage from "../images/ebook-example.jpg";
+import defaultPhoto from "../images/avatar.webp";
 function ProjectPreview({ data }) {
   return (
     <>
-                <section className="preview">
-        <div className="projectImage" style={{ backgroundImage: `url(${data.image})` }}></div>
+      <section className="preview">
+        <div
+          className="projectImage"
+          style={{
+            backgroundImage: `url(${data.image || defaultImage})`,
+          }}
+        ></div>
         <article className="card">
-          <h2 className="card__projectTitle"><span className="card__projectTitle--text">Personal project card</span></h2>
+          <h2 className="card__projectTitle">
+            <span className="card__projectTitle--text">
+              Personal project card
+            </span>
+          </h2>
 
           <div className="card__author">
-            <div className="card__authorPhoto"></div>
-            <p className="card__job">
-              {data.job || "Full stack Developer"} 
-            </p>
+            <div
+              className="card__authorPhoto"
+              style={{
+                backgroundImage: `url(${data.photo || defaultPhoto})`,
+              }}
+            ></div>
+            <p className="card__job">{data.job || "Full stack Developer"}</p>
             <h3 className="card__name">{data.author || "Emmelie Bjôrklund"}</h3>
           </div>
-      
-          <div className="card__project">            
+
+          <div className="card__project">
             <h3 className="card__name">{data.name || "Elegant Workspace"}</h3>
-            <p className="card__slogan">{data.slogan || "Diseños Exclusivos" }</p>
+            <p className="card__slogan">
+              {data.slogan || "Diseños Exclusivos"}
+            </p>
             <h3 className="card__descriptionTitle">Product description</h3>
-            <p className="card__description">{data.description ||"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, quos? Itaque, molestias eveniet laudantium adipisci vitae ratione"}</p>
+            <p className="card__description">
+              {data.description ||
+                "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, quos? Itaque, molestias eveniet laudantium adipisci vitae ratione"}
+            </p>
 
             <div className="card__technicalInfo">
-              <p className="card__technologies">{data.technologies || "React JS - HTML - CSS"}</p>
-          
-              <a className="icon icon__www" href={data.demo || "#"} title="Haz click para ver el proyecto online">
+              <p className="card__technologies">
+                {data.technologies || "React JS - HTML - CSS"}
+              </p>
+
+              <a
+                className="icon icon__www"
+                href={data.demo || "#"}
+                title="Haz click para ver el proyecto online"
+              >
                 Web link
               </a>
-              <a className="icon icon__github" href={data.repo || "#"} title="Haz click para ver el código del proyecto">
+              <a
+                className="icon icon__github"
+                href={data.repo || "#"}
+                title="Haz click para ver el código del proyecto"
+              >
                 GitHub link
               </a>
             </div>
           </div>
         </article>
-      </section> 
+      </section>
     </>
   );
 }
