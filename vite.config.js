d458c9fd1e5d1db592cdc5fb72ead34f36bdade3
Angélica Gozálvez <angelicagh87@gmail.com>
2025-07-10 12:57:17 +0200

    color url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/project-promo-51-module-3-team-3/",
   plugins: [react()],
   server: {
     open: "/",
