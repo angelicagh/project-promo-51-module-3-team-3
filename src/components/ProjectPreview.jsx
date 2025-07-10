0ea429c472f756358586fd92650f416a8c061839
sandramarincodes <smarinfernan@gmail.com>
2025-07-10 11:42:24 +0200

    Update project card

--- src/components/ProjectPreview.jsx
+++ src/components/ProjectPreview.jsx
@@ -12,7 +12,7 @@ function ProjectPreview({ data }) {
           <article className="card">
             <h2 className="card__projectTitle">
               <span className="card__projectTitle--text">
-                Personal project card
+                Tarjeta proyecto personal
               </span>
             </h2>
 
@@ -23,29 +23,37 @@ function ProjectPreview({ data }) {
                   backgroundImage: `url(${data.photo})`,
                 }}
               ></div>
-              <p className="card__job">{data.job}</p>
-              <h3 className="card__name">{data.author}</h3>
+              <p className="card__job">{data.job || "Full Stack Developer"}</p>
+              <h3 className="card__name">{data.author || "Sylvia Johnson"}</h3>
             </div>
 
             <div className="card__project">
-              <h3 className="card__name">{data.name}</h3>
-              <p className="card__slogan">{data.slogan}</p>
-              <h3 className="card__descriptionTitle">Product description</h3>
-              <p className="card__description">{data.description}</p>
+              <h3 className="card__name">{data.name || "Your Next Book"}</h3>
+              <p className="card__slogan">
+                {data.slogan || "Comparte tu libro favorito"}
+              </p>
+              <h3 className="card__descriptionTitle">
+                Descripción del producto
+              </h3>
+              <p className="card__description">
+                {data.description || "Crea tu tarjeta literaria"}
+              </p>
 
               <div className="card__technicalInfo">
-                <p className="card__technologies">{data.technologies}</p>
+                <p className="card__technologies">
+                  {data.technologies || "HTML | CSS | React"}
+                </p>
 
                 <a
                   className="icon icon__www"
-                  href={data.demo}
+                  href={data.demo || "#"}
                   title="Haz click para ver el proyecto online"
                 >
                   Web link
                 </a>
                 <a
                   className="icon icon__github"
-                  href={data.repo}
+                  href={data.repo || "#"}
                   title="Haz click para ver el código del proyecto"
                 >
                   GitHub link
